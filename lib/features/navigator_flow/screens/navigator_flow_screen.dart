70ceaf644d9040676c6d86a0f0739cfe9315220c
helloguddn (PC helloguddn) <hellogudddn@naver.com>
2026-08-15 13:47:26 +0900

    feat: 산재처리 2단계 추가

--- lib/features/navigator_flow/screens/navigator_flow_screen.dart
+++ lib/features/navigator_flow/screens/navigator_flow_screen.dart
@@ -455,12 +455,14 @@ class _NavigatorFlowScreenState extends State<NavigatorFlowScreen> {
             ),
           ),
         ),
-        ImportButtonsBlock(:final sources) => ImportButtonsRow(
-          sources: sources,
-          imported: _importedSources,
-          lang: lang,
-          onImport: _handleImport,
-        ),
+        ImportButtonsBlock(:final sources, :final comingSoon) =>
+          ImportButtonsRow(
+            sources: sources,
+            imported: _importedSources,
+            lang: lang,
+            comingSoon: comingSoon,
+            onImport: _handleImport,
+          ),
         FormEditBlock(
           :final sections,
           :final documentTitle,
@@ -496,7 +498,6 @@ class _NavigatorFlowScreenState extends State<NavigatorFlowScreen> {
         WageCalcBlock() => WageCalcSection(
           scratch: _wageScratch,
           lang: lang,
-          accentColor: _accentColor,
           onAdvance: _next,
         ),
         // 임금 플로우에선 쓰이지 않는 블록.
